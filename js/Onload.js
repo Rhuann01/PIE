32573fbbee4bf0c27d3f44b902dc14c8f269f07f
Rhuann01 <rhuann2006@gmail.com>
2025-05-17 01:41:56 -0300

    Primeiro commit

---
+++ js/Onload.js
@@ -0,0 +1,11 @@
+window.onload = function () {
+  imagenCard();
+};
+
+function imagenCard() {
+  document.querySelector("#primex .perfil").style.backgroundImage ="url('/imgs/contexto.jpg')";
+  document.querySelector("#secundex .perfil").style.backgroundImage ="url('imgs/hitler.png')";
+  document.querySelector("#tercex .perfil").style.backgroundImage ="url('imgs/holocalsto.avif')";
+  document.querySelector("#quartex .perfil").style.backgroundImage ="url('imgs/Brasil.jpg')";
+  document.querySelector("#quintex .perfil").style.backgroundImage ="url('/imgs/bombas.jpg')";
+}
