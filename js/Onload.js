6a3b2525559aabe571a609c20e80fd74c335c11e
Rhuann01 <rhuann2006@gmail.com>
2025-05-27 01:42:05 -0300

    Atualização 1.0, ainda falta adcições de 3 slides, e animações.

--- js/Onload.js
+++ js/Onload.js
@@ -9,5 +9,15 @@ function imagenCard() {
   document.querySelector("#quartex .perfil").style.backgroundImage ="url('imgs/Brasil.jpg')";
   document.querySelector("#quintex .perfil").style.backgroundImage ="url('imgs/bombas.jpg')";
   document.querySelector(".start .nav-bar").style.backgroundImage ="url('imgs/war.jpg')";
+  document.querySelector("#ContextImgPri").style.backgroundImage ="url('imgs/soldados-nazistas.png')";
+  document.querySelector("#HitlherImgPri").style.backgroundImage ="url('imgs/Hitler-grup.png')";
+  document.querySelector("#HitlherImgSec").style.backgroundImage ="url('imgs/hitler.png')";
+  document.querySelector("#HolocaustoImgPri").style.backgroundImage ="url('imgs/HolocalatoPIE.png')";
+  document.querySelector("#BrazilImgPri").style.backgroundImage ="url('imgs/Cobra-esta-fumando-600x777.png')";
+  document.querySelector("#BrazilImgSec").style.backgroundImage ="url('imgs/Mulher.png')";
+  document.querySelector("#twoBrazilImgPri").style.backgroundImage ="url('imgs/Gertulho-vargas.png')";
+  document.querySelector("#BombaPri").style.backgroundImage ="url('imgs/597647-43718-hiroshima-2520y-2520nagasaki.png')";
+  document.querySelector("#Oppenheimer").style.backgroundImage ="url('imgs/Openheimer.jpg')";
+
 }
 
